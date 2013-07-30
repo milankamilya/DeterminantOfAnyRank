4295ef65919bef22dd0b4f7203f34cc5cfc7ed24
Sandip Saha <innofied@Sandips-Mac-mini-2.local>
2013-07-30 16:44:31 +0530

    Initial Commit

---
+++ DeterminantOfAnyRank/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DeterminantOfAnyRank
+//
+//  Created by Sandip Saha on 30/07/13.
+//  Copyright (c) 2013 Sandip Saha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
